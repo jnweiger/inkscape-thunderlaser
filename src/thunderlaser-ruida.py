e1c97bc90cc4e26f1e32901a5400350f3242f5cf
Juergen Weigert <jw@owncloud.com>
2017-12-09 17:08:18 +0100

    credits updated and version number logic defined.

--- src/thunderlaser-ruida.py
+++ src/thunderlaser-ruida.py
@@ -33,7 +33,7 @@ if sys.version_info.major < 3:
 
 class ThunderLaser(inkex.Effect):
 
-    __version__ = '0.1'
+    __version__ = '1.1' # >= max(src/ruida.py:__version__, src/inksvg.py:__version__)
 
     def __init__(self):
         inkex.localize()    # does not help for localizing my *.inx file
