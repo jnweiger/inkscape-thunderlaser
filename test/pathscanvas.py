298b0d7dff3185fb0f62593b9c6db551e0fe5b65
Juergen Weigert <jw@owncloud.com>
2017-12-19 17:29:11 +0100

    incremental mode added.

--- test/pathscanvas.py
+++ test/pathscanvas.py
@@ -9,16 +9,30 @@
 import sys, json, gtk, random
 from goocanvas import *
 
+numbers=False           # set to True to add node numbers to nodes.
+incremental=0           # color the path, when SPACE pressed.
 
 ### window, canvas and navigation setup:
 
 def key_press(win, ev, c):
-  new_idx = None
+  global incremental
+
   s = c.get_scale()
   key = chr(ev.keyval & 0xff)
   if   key == '+':  c.set_scale(s*1.2)
   elif key == '-':  c.set_scale(s*.8)
-  elif ev.keyval <= 255: gtk.main_quit()
+  elif key == ' ':
+    show_poly(root, path=data['paths'][incremental], xoff=30, yoff=50, idx=incremental, color="red", width=0.3)
+    if incremental < len(data['paths'])-1:
+      incremental += 1
+  elif ev.keyval in (65288, 65535, 65507):      # bs, del, ctrl-h
+    if incremental > 0:
+      incremental -= 1
+    show_poly(root, path=data['paths'][incremental], xoff=30, yoff=50, idx=incremental, color="black", width=0.3)
+  elif ev.keyval <= 255:
+    gtk.main_quit()
+  else:
+    print("undefined key: " + str(ev.keyval))
 
 def button_press(win, ev):
   win.click_x = ev.x
@@ -60,24 +74,26 @@ def translate_poly(poly,xoff,yoff,scale=1):
   for i in poly: tuplepath.append( tuple([i[0]*scale+xoff, i[1]*scale+yoff]) )
   return tuplepath
 
-def show_poly(canvas, path = [(0,0),(20,0),(10,20),(0,0)], xoff=0, yoff=0, idx=1 ):
+def show_poly(canvas, path = [(0,0),(20,0),(10,20),(0,0)], xoff=0, yoff=0, idx=1, width=0.2, color="black"):
   """ default path is a downward pointing triangle.
       Both, a list of lists, and a list of tuples is accepted.
   """
   tuplepath=translate_poly(path, xoff, yoff)
   p = Points(tuplepath)         # cannot handle 2-element lists, need 2-element tuples.
-  poly = Polyline(parent=canvas, points=p, line_width=0.2, stroke_color="black")
+  poly = Polyline(parent=canvas, points=p, line_width=width, stroke_color=color)
 
-  for C in path:
-    text = Text(parent=canvas, text=idx, font="4", fill_color="blue")
-    idx += 1
-    text.translate(C[0]+xoff+random.uniform(-.5,0), C[1]+yoff+random.uniform(-.5,0))
-    text.scale(.25,.25)
+  if idx is not None:
+    for C in path:
+      text = Text(parent=canvas, text=idx, font="4", fill_color="blue")
+      idx += 1
+      text.translate(C[0]+xoff+random.uniform(-.5,0), C[1]+yoff+random.uniform(-.5,0))
+      text.scale(.15,.15)
 
 data = json.load(open(sys.argv[1]))
 idx = 1
+data['paths'] = data['mark']['paths'] + data['cut']['paths']
 for path in data['paths']:
-  show_poly(root, path=path, xoff=30, yoff=50, idx=idx)
+  show_poly(root, path=path, xoff=30, yoff=50, idx=None)
   idx += len(path)
 
 # show_poly(root, path = [(0,0),(20,0),(10,20),(0,0)], xoff=30, yoff=50)
